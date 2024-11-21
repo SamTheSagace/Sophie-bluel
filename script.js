07e44fc339863a9e27d9c70d90372557eedcd87e
SaulofTarse <74056270+SaulofTarse@users.noreply.github.com>
2024-11-21 19:37:52 +0100

    Cleaning up
    Cleaned up the gallery generation / filter functions

--- script.js
+++ script.js
@@ -1,94 +1,99 @@
 
 let dataWork = ''
-
+// fetching the data for the works
 async function getWork(){
     const response = await fetch("http://localhost:5678/api/works");
     return await response.json(); // Parse the JSON response if needed 
 }
 
-async function createGallery() {
-    try {        
-        dataWork = await getWork()
-        let allFigures =''
-        for (let i = 0; i< dataWork.length; i++ ){
-        let imgFigure = dataWork[i].imageUrl
-        let titreFigure = dataWork[i].title
-        let figure = `                
-            <figure>
-                <img src="${imgFigure}">
-                <figcaption>${titreFigure}</figcaption>
-            </figure>
-        `;
-         allFigures += figure;
-         
-        }
-        let gallery = document.getElementById("portfolio").querySelector(".gallery")
-        gallery.innerHTML = ''
-        gallery.innerHTML = allFigures
-        return gallery 
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-}
+// async function createfilters(){
+//     dataWork = await getWork()  
+//     console.log(dataWork.category.name);   
+// }
 
-createGallery();
-
-
+// let for the different button filter
 let buttonTrierTous = document.getElementById("tousButton")
 let buttonTrierAppartements = document.getElementById("appartementButton")
 let buttonTrierObjets = document.getElementById("objetsButton")
 let buttonTrierHotel = document.getElementById("hotelButton")
 let buttons = document.querySelectorAll(".button")
 
+// create the galllery at the start
+async function createGallery(){
+    if(buttonTrierTous.classList.contains("buttonHover") === false){
+        buttons.forEach(button => {
+            button.classList.remove("buttonHover");
+        });
+        buttonTrierTous.classList.add("buttonHover")
+        try {        
+            dataWork = await getWork()
+            const objetsFiltrer = dataWork.filter(function (work) {
+                return work.category.name === "Objets";
+            });
+            let allFigures =''
+            for (let i = 0; i< dataWork.length; i++ ){
+            let figure = `                
+                <figure>
+                    <img src="${dataWork[i].imageUrl}">
+                    <figcaption>${dataWork[i].title}</figcaption>
+                </figure>
+            `;
+            allFigures += figure;
+            }
+            let gallery = document.getElementById("portfolio").querySelector(".gallery")
+            gallery.innerHTML = allFigures
+            return gallery 
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+    else{
+    }
+}
+createGallery();
+
+// when each button is pressed, it recreate the gallery with a filtered version of it
+
+async function createFilterGallery(nomButton, nomFiltre){
+    if(nomButton.classList.contains("buttonHover") === false){
+        buttons.forEach(button => {
+            button.classList.remove("buttonHover");
+        });
+        nomButton.classList.add("buttonHover")   
+        const Filtrer = dataWork.filter(function (work) {
+        return work.category.name === nomFiltre;
+        }); 
+        let allFigures =''
+        for (let i = 0; i< Filtrer.length; i++ ){
+            let figure = `                
+                <figure>
+                    <img src="${Filtrer[i].imageUrl}">
+                        <figcaption>${Filtrer[i].title}</figcaption>
+                </figure>
+            `;
+            allFigures += figure;
+        }
+        let gallery = document.getElementById("portfolio").querySelector(".gallery")
+        gallery.innerHTML = ''
+        gallery.innerHTML = allFigures  
+    }
+    else{
+    }
+}
+
 
 buttonTrierTous.addEventListener("click", async function() {
-    buttons.forEach(button => {
-        button.classList.remove("buttonHover");
-    });
-    buttonTrierTous.classList.add("buttonHover")
-    await createGallery();    
+     await createGallery();    
 }); 
 
 buttonTrierObjets.addEventListener("click", async function() {
-    buttons.forEach(button => {
-        button.classList.remove("buttonHover");
-    });
-    buttonTrierObjets.classList.add("buttonHover")
-    dataWork = await getWork()
-    const objetsFiltrer = dataWork.filter(function (work) {
-        console.log(work.category.name); // Log category name
-        return work.category.name === "Objets";
-    });
-    let allFigures =''
-    for (let i = 0; i< objetsFiltrer.length; i++ ){
-    let imgFigure = objetsFiltrer[i].imageUrl
-    let titreFigure = objetsFiltrer[i].title
-    let figure = `                
-        <figure>
-            <img src="${imgFigure}">
-            <figcaption>${titreFigure}</figcaption>
-        </figure>
-    `;
-    allFigures += figure;
-    }
-    let gallery = document.getElementById("portfolio").querySelector(".gallery")
-    gallery.innerHTML = ''
-    gallery.innerHTML = allFigures
+    await createFilterGallery(buttonTrierObjets, "Objets")
 }); 
 
-
 buttonTrierAppartements.addEventListener("click", async function() {
-    buttons.forEach(button => {
-        button.classList.remove("buttonHover");
-    });
-    buttonTrierAppartements.classList.add("buttonHover")
-    await createGallery();
+    await createFilterGallery(buttonTrierAppartements, "Appartements")
 }); 
 
 buttonTrierHotel.addEventListener("click", async function() {
-    buttons.forEach(button => {
-        button.classList.remove("buttonHover");
-    });
-    buttonTrierHotel.classList.add("buttonHover")
-    await createGallery();
+    await createFilterGallery(buttonTrierHotel, "Hotels & restaurants")
 }); 
